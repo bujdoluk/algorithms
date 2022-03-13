04ea3db34c8f1b2e9145ae5c041d05d129764fa8
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2022-03-13 14:05:56 +0100

    selection sort

--- sortingAlgorhitms.js
+++ sortingAlgorhitms.js
@@ -2,7 +2,11 @@
 let container = document.getElementById("canvas");
 let bubbleSortButton = document.getElementById("bubbleSort");
 let generateButton = document.getElementById("generate");
-let binarySearchButton = document.getElementById("binarySearch");
+let selectionSortButton = document.getElementById("selectionSort");
+
+generateButton.addEventListener("click", generate);
+bubbleSortButton.addEventListener("click", BubbleSort);
+selectionSortButton.addEventListener("click", SelectionSort);
 
 function generateRandomArray() {
     for (let i = 0; i < 20; i++) {
@@ -10,13 +14,13 @@ function generateRandomArray() {
 
         let element = document.createElement("div");
 
-        element.classList.add("block");
+        element.classList.add("bar");
 
         element.style.height = `${number * 3}px`;
         element.style.transform = `translate(${i * 40}px)`;
 
         let element_label = document.createElement("label");
-        element_label.classList.add("block_label");
+        element_label.classList.add("bar_label");
         element_label.innerText = number;
 
         element.appendChild(element_label);
@@ -35,18 +39,18 @@ function swap(element1, element2) {
             setTimeout(() => {
                 container.insertBefore(element2, element1);
                 resolve();
-            }, 100)
+            }, 250)
         })
     })
 }
 
 async function BubbleSort(delay = 100) {
-    var blocks = document.querySelectorAll(".block");
+    var bars = document.querySelectorAll(".bar");
 
-    for (i = 0; i < blocks.length; i++) {
-        for (j = 0; j < blocks.length - 1 - i; j++) {
-            blocks[j].style.backgroundColor = "red";
-            blocks[j + 1].style.backgroundColor = "red";
+    for (let i = 0; i < bars.length; i++) {
+        for (let j = 0; j < bars.length - 1 - i; j++) {
+            bars[j].style.backgroundColor = "red";
+            bars[j + 1].style.backgroundColor = "red";
 
             await new Promise((resolve) => {
                 setTimeout(() => {
@@ -54,30 +58,80 @@ async function BubbleSort(delay = 100) {
                 }, delay)
             });
 
-            let number1 = Number(blocks[j].childNodes[0].innerHTML);
-            let number2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+            let number1 = Number(bars[j].childNodes[0].innerHTML);
+            let number2 = Number(bars[j + 1].childNodes[0].innerHTML);
 
             if (number1 > number2) {
-                await swap(blocks[j], blocks[j + 1]);
-                blocks = document.querySelectorAll(".block");
+                await swap(bars[j], bars[j + 1]);
+                bars = document.querySelectorAll(".bar");
             }
 
-            blocks[j].style.backgroundColor = "blue";
-            blocks[j + 1].style.backgroundColor = "blue";
+            bars[j].style.backgroundColor = "blue";
+            bars[j + 1].style.backgroundColor = "blue";
         }
 
-        blocks[blocks.length - 1 - i].style.backgroundColor = "green";
+        bars[bars.length - 1 - i].style.backgroundColor = "rgb(49, 226, 13)";
     }
 }
 
-function binarySearch() {
+async function SelectionSort(delay = 100) {
+    let bars = document.querySelectorAll(".bar");
+
+    let min_index = 0;
+    for (let i = 0; i < bars.length; i++) {
+        min_index = i;
+
+        bars[i].style.backgroundColor = "darkblue";
+        for (let j = i + 1; j < bars.length; j++) {
+            bars[j].style.backgroundColor = "red";
+
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, 100)
+            );
 
 
+            let number1 = parseInt(bars[j].childNodes[0].innerHTML);
+            let number2 = parseInt(bars[min_index].childNodes[0].innerHTML);
+
+            if (number1 < number2) {
+                if (min_index !== i) {
+                    bars[min_index].style.backgroundColor = "rgb(24, 190, 255)";
+                }
+                min_index = j;
+            } else {
+                bars[j].style.backgroundColor = "rgb(24, 190, 255)";
+            }
+        }
+
+        let temp1 = bars[min_index].style.height;
+        let temp2 = bars[min_index].childNodes[0].innerText;
+        bars[min_index].style.height = bars[i].style.height;
+        bars[i].style.height = temp1;
+        bars[min_index].childNodes[0].innerText = bars[i].childNodes[0].innerText;
+        bars[i].childNodes[0].innerText = temp2;
+
+        await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, 250)
+        );
+
+        bars[min_index].style.backgroundColor = "rgb(24, 190, 255)";
+        bars[i].style.backgroundColor = "rgb(49, 226, 13)";
+
+
+    }
 }
 
-generateButton.addEventListener("click", generateRandomArray);
-bubbleSortButton.addEventListener("click", BubbleSort);
-binarySearchButton.addEventListener("click", binarySearch);
+function generate() {
+    window.location.reload();
+}
+
+generateRandomArray();
+
+
 
 
 
